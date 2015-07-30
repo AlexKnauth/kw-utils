c142bb900178f16f6703d3ad686a7fd9792ba0f9
AlexKnauth <alexander@knauth.org>
2015-07-30 17:55:40 -0400

    add missing dep

--- info.rkt
+++ info.rkt
@@ -4,5 +4,5 @@
 
 (define deps '("base" "sweet-exp" "my-cond"))
 
-(define build-deps '("rackunit-lib" "scribble-lib" "racket-doc"))
+(define build-deps '("rackunit-lib" "scribble-lib" "racket-doc" "rackjure"))
 
