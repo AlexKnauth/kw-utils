ac1edc0d53ab121c4e88d4aff8b44523753c325d
AlexKnauth <alexander@knauth.org>
2016-09-04 22:09:37 -0400

    fix define-kw-proc to avoid procedure-rename
    see
    https://github.com/AlexKnauth/kw-utils/issues/3#issuecomment-244644654

--- kw-utils/partial.rkt
+++ kw-utils/partial.rkt
@@ -7,6 +7,7 @@
 (require racket/list
          "arity+keywords.rkt"
          "keyword-app.rkt"
+         "kw-lists-lambda.rkt"
          )
 (module+ test
   (require rackunit))
@@ -15,21 +16,20 @@
 ;; https://github.com/greghendershott/rackjure/blob/master/rackjure/utils.rkt
 ;; http://pkg-build.racket-lang.org/doc/rackjure/index.html#%28def._%28%28lib._rackjure%2Futils..rkt%29._partial%29%29
 
-(define (kw-proc name arity+kws proc)
-  (procedure-reduce-arity+keywords
-   (procedure-rename (make-keyword-procedure proc) name)
-   arity+kws))
-
-(define-syntax-rule (define-kw-proc name arity+kws clause ...)
-  (define name (kw-proc 'name arity+kws (case-lambda clause ...))))
+(define-syntax-rule (define-kw-proc name kws-id kw-args-id #:arity arity+kws clause ...)
+  (define name
+    (let ([ar+kws arity+kws]
+          [name (kw-lists-case-lambda kws-id kw-args-id clause ...)])
+      (procedure-reduce-arity+keywords name ar+kws))))
 
 (define (double-keyword-apply f kws-1 kw-args-1 kws-2 kw-args-2 rest-args)
   (keyword-app keyword-apply kws-1 kw-args-1
                f kws-2 kw-args-2
                rest-args))
 
-(define-kw-proc partial (arity+keywords (arity-at-least 0) '() #f)
-  [(kws-1 kw-args-1 f . args-1)
+(define-kw-proc partial kws-1 kw-args-1
+  #:arity (arity+keywords (arity-at-least 0) '() #f)
+  [(f . args-1)
    (define f.arity+kws
      (procedure-arity+keywords f))
    (define arity+kws
@@ -38,12 +38,13 @@
          [(empty? (arity+keywords-arity arity+kws))
           (raise-too-many-partial-arguments-error f kws-1 kw-args-1 args-1)]
          [else
-          (define-kw-proc partial-f arity+kws
-            [(kws-2 kw-args-2 . args-2)
+          (define-kw-proc partial-f kws-2 kw-args-2
+            #:arity arity+kws
+            [args-2
              (double-keyword-apply f kws-1 kw-args-1 kws-2 kw-args-2
                                    (append args-1 args-2))])
           partial-f])]
-  [(kws-1 kw-args-1)
+  [()
    (keyword-app partial kws-1 kw-args-1 app)])
 
 (define (raise-too-many-partial-arguments-error f kws-1 kw-args-1 args-1)
@@ -65,8 +66,9 @@
      (format "~a ~v " kw kw-arg))))
 
 
-(define-kw-proc app (arity+keywords (arity-at-least 1) '() #f)
-  [(kws kw-args f . args)
+(define-kw-proc app kws kw-args
+  #:arity (arity+keywords (arity-at-least 1) '() #f)
+  [(f . args)
    (keyword-apply f kws kw-args args)])
 
 
